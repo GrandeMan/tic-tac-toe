727b97af47c5955910577c2f20a92e813e8b137d
Grandeman <joshuajmorales01@gmail.com>
2023-09-19 15:03:50 -0400

    Create display

--- script.js
+++ script.js
@@ -4,10 +4,24 @@ const gameBoard = (() =>{
     
     const getBoard = () => board;
 
+    const markCell = (index, marker) => {
+        if (board[index] === '') {
+            board[index] = marker;
+            return true;
+        }
+        return false;
+    };
 
+    return {getBoard, markCell};
 })();
 
 //Player
 const Player = (name, marker) => {
     return {name, marker};
-}
+};
+
+//Display Controller
+const displayController = (() => {
+    const cells = document.querySelectorAll(".cell");
+    const messageDiv = document.getElementById("message");
+})();
